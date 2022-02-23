bbbff4fbffa43066468f2ba2b22eef181f6f70e7
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-02-23 18:14:59 -0300

    test app.post ok with insomnia

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
 
 // array para armazenamento provisório dos dados do cliente //
 const costumers = [];
