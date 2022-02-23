adbe8f48ac292bb4895a188eabb24682dff879e2
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-02-23 17:28:30 -0300

    added app.post method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+// array para armazenamento provisório dos dados do cliente //
+const costumers = [];
+
+// criar conta - método post //
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    costumers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
